34cf19317ef6eb2a144a766ea394e01cb712dea4
aveirahabeeb <aveirahabeeb@gmail.com>
2019-04-03 16:10:08 +0100

    Update AlternateC.java

--- AlternateC.java
+++ AlternateC.java
@@ -1,19 +1,20 @@
-﻿import java.util.Scanner;
+﻿import java.util.*;
+import java.io.*;
 
 public class Solution{
        public static void main(String[] args) {
-              Scanner sc = new Scanner(System.in);
-              int T = sc.nextInt();
-              for (int count = 0; count < T; count++) {
-                     char[] characters= sc.next().toCharArray();
-                      int AlterCount=0;
+              Scanner input = new Scanner(System.in);
+              int q = input.nextInt();
+              for (int count = 0; count < q; count++) {
+                     char[] characters= input.next().toCharArray();
+                      int delete=0;
                      for(int i=0;i<characters.length-1;i++) {
                            if(characters[i]==characters[i+1]) {
-                                  AlterCount++;
+                                  delete++;
                            }
                      }
-                     System.out.println(AlterCount);
+                     System.out.println(delete);
               }
-              sc.close();
+              input.close();
        }
 }
